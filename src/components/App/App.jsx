f3de585de2e90dfc322f9fe9aad6fbee7b2524c9
Maksym Solomko <max.exc7@gmail.com>
2023-01-20 15:27:22 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,36 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template 8 Final Task
+    <div>
+      <header style={{
+          height: '40px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}>
+      <navigation>HOME </navigation>
+      <div> 
+        <span>Register  </span> 
+        <span>Login</span> 
+      </div>
+     
+      </header>
+      <br />
+      <hr />
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React homework template 8 Final Task
+      </div>
     </div>
   );
 };
