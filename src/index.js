1dc42af3a7e57443b1bde640103b4f23dc1299c3
Maksym Solomko <max.exc7@gmail.com>
2023-01-23 18:00:24 +0200

    up

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
 // import { App } from 'components/App/App';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { Provider } from 'react-redux';
-// import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store} > */}
-      {/* <PersistGate loading={null} persistor={persistor} > */}
+    <Provider store={store} >
+      <PersistGate loading={null} persistor={persistor} >
         <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
-      {/* </PersistGate> */}
-    {/* </Provider> */}
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -32,63 +32,63 @@ const register = createAsyncThunk('auth/register', async credentials => {
  * body: { email, password }
  * После успешного логина добавляем токен в HTTP-заголовок
  */
-const logIn = createAsyncThunk('auth/login', async credentials => {
-  try {
-    const { data } = await axios.post('/users/login', credentials);
-    // token.set(data.token);
-    return data;
-  } catch (error) {
-    // TODO: Добавить обработку ошибки error.message
-  }
-});
+// const logIn = createAsyncThunk('auth/login', async credentials => {
+//   try {
+//     const { data } = await axios.post('/users/login', credentials);
+//     // token.set(data.token);
+//     return data;
+//   } catch (error) {
+//     // TODO: Добавить обработку ошибки error.message
+//   }
+// });
 
-/*
- * POST @ /users/logout
- * headers: Authorization: Bearer token
- * После успешного логаута, удаляем токен из HTTP-заголовка
- */
-const logOut = createAsyncThunk('auth/logout', async () => {
-  try {
-    await axios.post('/users/logout');
-    // token.unset();
-  } catch (error) {
-    // TODO: Добавить обработку ошибки error.message
-  }
-});
-/*
- * GET @ /users/current
- * headers:
- *    Authorization: Bearer token
- *
- * 1. Забираем токен из стейта через getState()
- * 2. Если токена нет, выходим не выполняя никаких операций
- * 3. Если токен есть, добавляет его в HTTP-заголовок и выполянем операцию
- */
-const fetchCurrentUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const persistedToken = state.auth.token;
+// /*
+//  * POST @ /users/logout
+//  * headers: Authorization: Bearer token
+//  * После успешного логаута, удаляем токен из HTTP-заголовка
+//  */
+// const logOut = createAsyncThunk('auth/logout', async () => {
+//   try {
+//     await axios.post('/users/logout');
+//     // token.unset();
+//   } catch (error) {
+//     // TODO: Добавить обработку ошибки error.message
+//   }
+// });
+// /*
+//  * GET @ /users/current
+//  * headers:
+//  *    Authorization: Bearer token
+//  *
+//  * 1. Забираем токен из стейта через getState()
+//  * 2. Если токена нет, выходим не выполняя никаких операций
+//  * 3. Если токен есть, добавляет его в HTTP-заголовок и выполянем операцию
+//  */
+// const fetchCurrentUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, thunkAPI) => {
+//     const state = thunkAPI.getState();
+//     const persistedToken = state.auth.token;
 
-    if (persistedToken === null) {
-      console.log('Токена нет, уходим из fetchCurrentUser');
-      return thunkAPI.rejectWithValue();
-    }
+//     if (persistedToken === null) {
+//       console.log('Токена нет, уходим из fetchCurrentUser');
+//       return thunkAPI.rejectWithValue();
+//     }
 
-    // token.set(persistedToken);
-    try {
-      const { data } = await axios.get('/users/current');
-      return data;
-    } catch (error) {
-      // TODO: Добавить обработку ошибки error.message
-    }
-  },
-);
+//     // token.set(persistedToken);
+//     try {
+//       const { data } = await axios.get('/users/current');
+//       return data;
+//     } catch (error) {
+//       // TODO: Добавить обработку ошибки error.message
+//     }
+//   },
+// );
 
 const authOperations = {
   register,
-  logOut,
-  logIn,
-  fetchCurrentUser,
+//   logOut,
+//   logIn,
+//   fetchCurrentUser,
 };
 export default authOperations;
