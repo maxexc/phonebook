79d7700bc13e7493a523a7c2af20eeb28ef296d5
Maksym Solomko <max.exc7@gmail.com>
2023-01-24 18:31:56 +0200

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { register, logIn, logOut, refreshUser } from './operations';
+// import { register, logIn, logOut, refreshUser } from './operations';
 
-const initialState = {
-  user: { name: null, email: null },
-  token: null,
-  isLoggedIn: false,
-  isRefreshing: false,
-};
+// const initialState = {
+//   user: { name: null, email: null },
+//   token: null,
+//   isLoggedIn: false,
+//   isRefreshing: false,
+// };
 
 const authSlice = createSlice({
   name: 'auth',
