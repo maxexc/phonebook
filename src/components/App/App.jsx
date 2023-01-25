926d9a9c9aed716fa3dd5724ba808ab90e46fd22
Maksym Solomko <max.exc7@gmail.com>
2023-01-25 23:35:35 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,19 +3,20 @@ import { Layout } from 'components/Layout/Layout';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/auth/operations';
-import  useAuth  from 'hooks/useAuth';
+// import  useAuth  from 'hooks/useAuth';
 
 
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import Contacts from 'pages/Contacts';
+import { useAuth } from 'hooks';
 
 
 
 const App = () => {
   const dispatch = useDispatch();
-  const { isRefreshing } = useAuth();
+  const { isRefreshing } = useAuth;
 
   useEffect(() => {
     dispatch(refreshUser())
