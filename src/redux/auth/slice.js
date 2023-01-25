9cf3030971e5ca95b477c22992d8faa77e2853fa
Maksym Solomko <max.exc7@gmail.com>
2023-01-25 03:35:11 +0200

    Revert "Update slice.js"
    This reverts commit cae8bfe407f88f7060ced6ed8e08560da5c831a3.

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -23,8 +23,6 @@ const authSlice = createSlice({
         .addCase(register.fulfilled, (state, action) => {
           state.user = action.payload.user;
           state.token = action.payload.token;
-          state.isLoggedIn = true;
-
         })
         .addCase(register.rejected, (state, action) => state)
     ,
