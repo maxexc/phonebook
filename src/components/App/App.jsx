401f69c4759106da921f0a366ffa2f24dde7066e
Maksym Solomko <max.exc7@gmail.com>
2023-01-26 13:30:15 +0200

    up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react'
 import { Layout } from 'components/Layout/Layout';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 import { refreshUser } from 'redux/auth/operations';
-// import { useAuth } from 'hooks/useAuth';
+import { useAuth } from 'hooks/useAuth';
 
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import Contacts from 'pages/Contacts';
-import { selectIsRefreshing } from 'redux/auth/selectors';
+// import { selectIsRefreshing } from 'redux/auth/selectors';
 
 
 const App = () => {
   const dispatch = useDispatch();
-  // const { isRefreshing } = useAuth();
-  const isRefreshing = useSelector(selectIsRefreshing);
+  const { isRefreshing } = useAuth();
+  // const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser())
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,14 +1,15 @@
 import { AuthNav } from "components/AuthNav/AuthNav";
 import Navigation from "components/Navigation/Navigation";
 import { UserMenu } from "components/UserMenu/UserMenu";
-// import { useAuth } from "hooks";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "redux/auth/selectors";
+import { useAuth } from "hooks";
+// import { useSelector } from "react-redux";
+// import { selectIsLoggedIn } from "redux/auth/selectors";
 import { Header } from "./AppBar.styled";
 
 
 export const AppBar = () => {
-    const { isLoggedIn } = useSelector(selectIsLoggedIn);
+    // const { isLoggedIn } = useSelector(selectIsLoggedIn);
+  const { isLoggedIn } = useAuth();
 
     console.log(isLoggedIn);
   
