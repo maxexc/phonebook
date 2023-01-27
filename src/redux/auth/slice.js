69625d57d70c234d15c9335881d210ac152095af
Maksym Solomko <max.exc7@gmail.com>
2023-01-27 11:19:44 +0200

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -16,6 +16,7 @@ const authSlice = createSlice({
       isLoggedIn: false,
       isRefreshing: false,
     },
+    
     extraReducers: builder =>
       builder
         .addCase(register.pending, (state, action) => state)
@@ -51,37 +52,33 @@ const authSlice = createSlice({
 
 export const authReducer = authSlice.reducer;
 
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   extraReducers: {
-//     [register.fulfilled](state, action) {
-//       state.user = action.payload.user;
-//       state.token = action.payload.token;
-//       state.isLoggedIn = true;
-//     },
-//     [logIn.fulfilled](state, action) {
-//       state.user = action.payload.user;
-//       state.token = action.payload.token;
-//       state.isLoggedIn = true;
-//     },
-//     [logOut.fulfilled](state) {
-//       state.user = { name: null, email: null };
-//       state.token = null;
-//       state.isLoggedIn = false;
-//     },
-//     [refreshUser.pending](state) {
-//       state.isRefreshing = true;
-//     },
-//     [refreshUser.fulfilled](state, action) {
-//       state.user = action.payload;
-//       state.isLoggedIn = true;
-//       state.isRefreshing = false;
-//     },
-//     [refreshUser.rejected](state) {
-//       state.isRefreshing = false;
-//     },
+// extraReducers: {
+//   [register.fulfilled](state, action) {
+//     state.user = action.payload.user;
+//     state.token = action.payload.token;
+//     state.isLoggedIn = true;
+//     // toast.success('Your account was successfully created');
 //   },
-// });
-
-// export const authReducer = authSlice.reducer;
+//   [register.rejected]() {
+//     // toast.error('User creation error. Try again with another email address');
+//   },
+//   [logIn.fulfilled](state, action) {
+//     state.user = action.payload.user;
+//     state.token = action.payload.token;
+//     state.isLoggedIn = true;
+//     // toast.success(`Welcome ${action.payload.user.name}`);
+//   },
+//   [logIn.rejected]() {
+//     // toast.error('Incorrect email or password! Please try again');
+//   },
+//   [logOut.fulfilled](state) {
+//     state.user = { name: null, email: null };
+//     state.token = null;
+//     state.isLoggedIn = false;
+//     // toast.success('Goodbye')
+//   },
+//   [refreshUser.fulfilled](state, action) {
+//     state.user = action.payload;
+//     state.isLoggedIn = true;
+//   },
+// }
