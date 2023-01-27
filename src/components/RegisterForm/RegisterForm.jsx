a27b7a4293c90553db673832fc1f8a23753cff6e
Maksym Solomko <max.exc7@gmail.com>
2023-01-27 19:57:06 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 import { Label, RegForm } from './RegisterForm.styled';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = event => {
+    switch (event.currentTarget.name) {
+      case "name": setName(event.currentTarget.value);
+        break;
+  
+      case "email": setEmail(event.currentTarget.value);
+        break;
+
+      case 'password': setPassword(event.currentTarget.value);
+        break;
+  
+      default:
+        return;
+    };
+    console.log(name, email, password);
+}
+      
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     dispatch(
       register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
+        name: form.elements.name.value.trim(),
+        email: form.elements.email.value.toLowerCase().trim(),
+        password: form.elements.password.value.trim(),
       })
     );
     form.reset();
@@ -22,15 +44,45 @@ export const RegisterForm = () => {
     <RegForm  onSubmit={handleSubmit} autoComplete="off">
       <Label >
         Username
-        <input type="text" name="name" />
+        <input 
+          type="text"
+          name="name"
+          id={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          placeholder="Emmy Richards"
+          required
+          value={name}
+          onChange={handleChange}          
+        />
       </Label>
       <Label >
         Email
-        <input type="email" name="email" />
+        <input 
+          type="email"
+          name="email"
+          id={email}
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Email must contain at list '@'. For example user@mail.com"
+          placeholder="user@mail.com"
+          required
+          value={email}
+          onChange={handleChange}
+        />
       </Label>
       <Label >
         Password
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          id={password}
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Password must be at least 7 characters long"
+          placeholder="password"
+          required
+          value={password}
+          onChange={handleChange}  
+        />
       </Label>
       <button type="submit">Register</button>
     </RegForm>
