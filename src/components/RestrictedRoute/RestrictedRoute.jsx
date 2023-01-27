e401551862525851aa10e311d2c27367f5616b33
Maksym Solomko <max.exc7@gmail.com>
2023-01-27 11:33:12 +0200

    Create RestrictedRoute.jsx

---
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -0,0 +1,7 @@
+import { useAuth } from "Hooks"
+import { Navigate } from "react-router-dom";
+
+export const RestrictedRoute = ({component: Component, redirectTo = '/'}) => {
+    const {isLoggedIn} = useAuth();
+    return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+};
