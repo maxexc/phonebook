a2bbefdf484a11c3b7bacb6521bb7e9880454338
Maksym Solomko <max.exc7@gmail.com>
2024-10-05 21:41:35 +0200

    update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,13 +3,11 @@ import Loader from "components/Loader/Loader"
 import { Suspense } from "react"
 import { Toaster } from "react-hot-toast"
 import { Outlet } from "react-router-dom"
-// import TsparticlesLayout from "components/TsparticlesLayout/TsparticlesLayout";
 
 
 export const Layout = () => {
     return (
         <>
-            {/* <TsparticlesLayout /> */}
             <AppNavBar />
             <Suspense fallback={<Loader />}>
                 <Outlet />
